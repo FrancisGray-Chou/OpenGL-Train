aca49d1299851e6082df9b41d5ec30b7067ac29b
FrancisGray-Chou <FrancisGray233@outlook.com>
2024-06-12 14:01:22 +0800

    Update fragment.fs

--- fragment.fs
+++ fragment.fs
@@ -1,9 +1,15 @@
 #version 330 core
-
 out vec4 FragColor;
+
 in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform float mixValue;
+
+uniform sampler2D ourTexture1;
+uniform sampler2D ourTexture2;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, TexCoord), mixValue);
 }
